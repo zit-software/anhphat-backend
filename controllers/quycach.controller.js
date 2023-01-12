4b6bc4c02284e62c9dc55c008680b31e87429ad7
TranHuy2k2 <giahuytran2002@gmail.com>
2023-01-12 19:47:26 +0700

    updated quycach

--- controllers/quycach.controller.js
+++ controllers/quycach.controller.js
@@ -1,5 +1,5 @@
-const { Sequelize } = require("sequelize");
 const DonViModel = require("~/models/donvi.model");
+const LoaiHangModel = require("~/models/loaihang.model");
 const QuyCachModel = require("~/models/quycach.model");
 
 class QuyCachController {
@@ -15,13 +15,18 @@ class QuyCachController {
 			);
 
 			const dv1 = await DonViModel.findOne({
-				attributes: ["ma", "ten"],
+				attributes: ["ma", "ten", "malh"],
 				where: { ma: req.body.madv1 },
 			});
 			const dv2 = await DonViModel.findOne({
-				attributes: ["ma", "ten"],
+				attributes: ["ma", "ten", "malh"],
 				where: { ma: req.body.madv2 },
 			});
+
+			if (dv1.dataValues.malh != dv2.dataValues.malh)
+				throw new Error(
+					"Không thể quy đổi đơn vị giữa hai loại hàng khác nhau"
+				);
 
 			if (!dv1 || !dv2)
 				throw new Error("Không tồn tại đơn vị");
@@ -46,25 +51,57 @@ class QuyCachController {
 	async laytatcaquycach(req, res) {
 		try {
 			const allQuyCach = await QuyCachModel.findAll({
-				attributes: ["soluong", "madv1", "madv2"],
+				attributes: [
+					"ma",
+					"soluong",
+					"madv1",
+					"madv2",
+				],
 			});
 			const result = [];
 			for (let quycach of allQuyCach) {
 				let dv1 = await DonViModel.findOne({
 					attributes: ["ma", "ten"],
 					where: { ma: quycach.dataValues.madv1 },
+					include: {
+						model: LoaiHangModel,
+						attributes: ["ma", "ten"],
+					},
 				});
 				let dv2 = await DonViModel.findOne({
 					attributes: ["ma", "ten"],
 					where: { ma: quycach.dataValues.madv2 },
 				});
+				let loaihang = dv1.dataValues.loaihang;
 				result.push({
-					dv1: dv1.dataValues,
+					ma: quycach.dataValues.ma,
+					lh: loaihang,
+					dv1: {
+						ma: dv1.dataValues.ma,
+						ten: dv1.dataValues.ten,
+					},
 					dv2: dv2.dataValues,
 					soluong: quycach.soluong,
 				});
 			}
 			return res.status(200).json(result);
+		} catch (error) {
+			res.status(400).send({
+				message: error.message,
+			});
+		}
+	}
+	async updateQuyCach(req, res) {
+		try {
+			const ma = req.params.ma;
+			await QuyCachModel.update(req.body, {
+				where: {
+					ma,
+				},
+			});
+			return res.status(200).json({
+				message: "Cập nhật thành công",
+			});
 		} catch (error) {
 			res.status(400).send({
 				message: error.message,
