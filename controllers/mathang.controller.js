17fb207e00665692c1b1bf9be1b1a0f13636709a
TranHuy2k2 <giahuytran2002@gmail.com>
2023-01-14 19:03:27 +0700

    updated mathang

--- controllers/mathang.controller.js
+++ controllers/mathang.controller.js
@@ -1,6 +1,8 @@
 const MatHangModel = require("~/models/mathang.model");
 const sequelize = require("~/services/sequelize.service");
 const { Op } = require("sequelize");
+const LoaiHangModel = require("~/models/loaihang.model");
+const DonViModel = require("~/models/donvi.model");
 class MathangController {
 	/**
 	 *
@@ -27,7 +29,8 @@ class MathangController {
 			const loaihangQuery = req.query.loaihang;
 			const donviQuery = req.query.donvi;
 			const ngaynhapQuery = req.query.ngaynhap;
-			let where = {
+			const orderQuery = req.query.order;
+			const where = {
 				daxuat: { [Op.ne]: null },
 			};
 			if (loaihangQuery) where.malh = loaihangQuery;
@@ -36,7 +39,19 @@ class MathangController {
 				where.ngaynhap = {
 					[Op.eq]: ngaynhapQuery,
 				};
-			console.log(where);
+			const order = [];
+			if (orderQuery == "hsd")
+				order.push([orderQuery, "ASC"]);
+			else if (orderQuery == "ngaynhap")
+				order.push([orderQuery, "DESC"]);
+			else if (orderQuery == "soluong")
+				order.push([orderQuery, "ASC"]);
+
+			const limit = req.query.page ? 2 : null;
+			const offset = limit
+				? limit * (req.query.page - 1)
+				: 0;
+
 			// Lấy tất cả mặt hàng
 			const allMH = await MatHangModel.findAll({
 				attributes: [
@@ -48,7 +63,7 @@ class MathangController {
 					[
 						sequelize.fn(
 							"count",
-							sequelize.col("ma")
+							sequelize.col("mathang.ma")
 						),
 						"soluong",
 					],
@@ -61,10 +76,38 @@ class MathangController {
 					"gianhap",
 				],
 				where,
+				include: [
+					{
+						model: LoaiHangModel,
+						attributes: [
+							"gianhap",
+							"giaban",
+							"ten",
+						],
+					},
+					{
+						model: DonViModel,
+						attributes: ["ma", "ten"],
+					},
+				],
+				order,
+				limit,
+				offset,
 			});
-			return res
-				.status(200)
-				.json(allMH.map((mh) => mh.toJSON()));
+			const total = await MatHangModel.count({
+				where,
+				group: [
+					"ngaynhap",
+					"hsd",
+					"malh",
+					"madv",
+					"gianhap",
+				],
+			});
+			return res.status(200).json({
+				data: allMH.map((mh) => mh.toJSON()),
+				total: total.length,
+			});
 		} catch (error) {
 			res.status(400).send({
 				message: error.message,
