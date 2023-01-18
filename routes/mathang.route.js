9763080a31e6450e76a80d10c0690b016fc35547
TranHuy2k2 <giahuytran2002@gmail.com>
2023-01-18 15:32:20 +0700

    updated api

--- routes/mathang.route.js
+++ routes/mathang.route.js
@@ -5,11 +5,16 @@ const {
 	laytatcamathang,
 	chinhsuamathang,
 	xoamathang,
+	laymotmathang,
 } = require("~/controllers/mathang.controller");
 
 const router = Router();
 
-router.route("/").post(themmathang).get(laytatcamathang);
+router
+	.route("/")
+	.post(themmathang)
+	.get(laytatcamathang)
+	.get(laymotmathang);
 
 router
 	.route("/:ma")
