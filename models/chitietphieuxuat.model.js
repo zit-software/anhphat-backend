e9a3cd1c2906c9b0bf41ec8135fe3f39a6b03053
TranHuy2k2 <giahuytran2002@gmail.com>
2023-01-31 18:50:41 +0700

    updated

--- models/chitietphieuxuat.model.js
+++ models/chitietphieuxuat.model.js
@@ -1,10 +1,17 @@
+const { DataTypes } = require("sequelize");
 const sequelize = require("~/services/sequelize.service");
 const MatHangModel = require("./mathang.model");
 const PhieuXuatModel = require("./phieuxuat.model");
 
 const ChiTietPhieuXuatModel = sequelize.define(
 	"chitietphieuxuat",
-	{},
+	{
+		tang: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+	},
 	{
 		timestamps: true,
 		paranoid: true,
